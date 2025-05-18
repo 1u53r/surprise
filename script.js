4907ffbc694c167a8afb689a1292f2bb899c81ab
1u53r <mtauheed8799@gmail.com>
2025-05-18 07:36:44 +0530

    images updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // --- Animated Banner Drop Effect ---
 const soloImages = [
-  // List all images in /solo except banner.png
+  // List all images in solo except banner.png
   "IMG_20241222_163044.jpg","IMG_20241222_163049.jpg","IMG_20241222_163051.jpg","IMG_20250101_162754.jpg","IMG_20250101_162805.jpg","IMG_20250103_183629.jpg","IMG_20250107_181445.jpg","IMG_20250107_181456 (1).jpg","IMG_20250107_181456.jpg","IMG-20241124-WA0017.jpg","IMG-20241205-WA0012.jpg","IMG-20241215-WA0017.jpg","IMG-20250119-WA0059.jpg","IMG-20250119-WA0122.jpg","IMG-20250201-WA0008 (1).jpg","IMG-20250202-WA0034.jpg","IMG-20250203-WA0004.jpg","IMG-20250203-WA0005.jpg","IMG-20250205-WA0018 (1).jpg","IMG-20250205-WA0046 (1).jpg","IMG-20250205-WA0067 (1).jpg","IMG-20250205-WA0073 (1).jpg","IMG-20250205-WA0075 (1).jpg","IMG-20250405-WA0008.jpg","IMG-20250405-WA0009.jpg","IMG-20250505-WA0012.jpg","IMG-20250505-WA0015.jpg","IMG-20250505-WA0023.jpg","IMG-20250505-WA0027.jpg","IMG-20250505-WA0028.jpg","IMG-20250505-WA0029.jpg","IMG-20250505-WA0030.jpg","IMG-20250505-WA0031.jpg","IMG-20250505-WA0033.jpg","IMG-20250505-WA0034.jpg","IMG-20250505-WA0035.jpg","IMG-20250505-WA0040.jpg","IMG-20250511-WA0001.jpg","Screenshot_20250123_000756.jpg","Screenshot_20250206_163109 (1).jpg","Screenshot_20250206_205609 (1).jpg","Screenshot_20250207_142032 (1).jpg","Snapchat-111262094 (1) (1).jpg","Snapchat-111262094 (1).jpg","Snapchat-1544830312 (1) (1).jpg","Snapchat-1544830312 (1).jpg","Snapchat-1613237512 (1) (1).jpg","Snapchat-1613237512 (1).jpg","Snapchat-1653172035 (1) (1).jpg","Snapchat-1653172035 (1).jpg","Snapchat-1742740907 (1).jpg","Snapchat-1800900428 (1).jpg","Snapchat-1875360662 (1).jpg","Snapchat-1904725201 (1).jpg","Snapchat-1996547810 (1).jpg","Snapchat-420669609 (1).jpg","Snapchat-425736260 (1).jpg","Snapchat-42926503 (1).jpg","Snapchat-565155465 (1).jpg","Snapchat-689951205 (1).jpg","Snapchat-755698779 (1).jpg","Snapchat-791963831 (1).jpg","Snapchat-914071737 (1).jpg","Snapchat-93611013 (1).jpg"
 ];
 
@@ -72,7 +72,7 @@ function animateBannerDrop() {
   const bannerHeight = window.innerHeight;
   soloImages.forEach((src, i) => {
     const img = document.createElement('img');
-    img.src = `/solo/${src}`;
+    img.src = `solo/${src}`;
     img.className = 'falling-image';
     img.style.position = 'absolute';
     img.style.width = '120px';
